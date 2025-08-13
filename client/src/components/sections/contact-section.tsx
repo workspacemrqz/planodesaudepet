675b1b95297c80de9373720f1c6fe0ca4f835765
g4brielblanc <46341486-g4brielblanc@users.noreply.replit.com>
2025-08-13 12:59:50 +0000

    Restored to '592f845c8b3c8ad25dd8e8183bd0f3df657720d2'
    Replit-Restored-To: 592f845c8b3c8ad25dd8e8183bd0f3df657720d2

--- client/src/components/sections/contact-section.tsx
+++ client/src/components/sections/contact-section.tsx
@@ -297,7 +297,7 @@ export default function ContactSection() {
             </div>
 
             {/* Service Hours */}
-            <Card className="unipet-card shadow-lg rounded-xl border-none w-fit">
+            <Card className="unipet-card shadow-lg rounded-xl border-none">
               <CardHeader className="flex flex-col space-y-1.5 p-6 bg-[#FBF9F7] text-[#ffffff] rounded-t-xl">
                 <CardTitle className="text-xl flex items-center tracking-tight text-[#277677] text-[20px] font-semibold">
                   <Clock className="h-5 w-5 mr-2" />
